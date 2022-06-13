f6327ff51802b99dcefb6ef8fb13394485cc8b9a
jt1230 <juliatang95@gmail.com>
2022-06-13 22:31:01 +0200

    newest retrievals uploaded

--- rain-on-africa/src/components/Home.jsx
+++ rain-on-africa/src/components/Home.jsx
@@ -1,5 +1,6 @@
 import './Home.css'
-import MOVIE from '../videos/2048.mp4'
+import FULL from '../videos/2048_week.mp4'
+import ANGOLA from '../videos/angola_week.mp4'
 
 function Home() {
 	return(
@@ -7,16 +8,17 @@ function Home() {
 			<div className="home-header">
 				<h1>Rain over Africa</h1>
 				<p>Demo of product</p>
+				<p>Animations show retrievals of precipitation rates over Africa for the first week in december 2021.</p>
 			</div>
 			<div className="fullcontinent">
 				<p>Retrievals over full continent</p>
-				<video src={MOVIE} width="1000" height="500" controls>
+				<video src={FULL} width="1000" height="500" controls>
 				Your browser does not support the video tag.
 				</video>
 			</div>
 			<div className="inregion">
 				<p>Retrievals over zoomed in region</p>
-				<video src={MOVIE} width="1000" height="500" controls>
+				<video src={ANGOLA} width="1000" height="500" controls>
 				Your browser does not support the video tag.
 				</video>
 			</div> 
